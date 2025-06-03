ec30e1ba86b582e5c317d630e964cdd246a40827
Prashanth Babu T <prashanthbabu07@gmail.com>
2025-06-03 10:36:09 +0530

    refactored

---
+++ RuleEngine.Solution.RuleEngine/Core/SimpleRule.cs
@@ -0,0 +1,11 @@
+﻿namespace RuleEngine;
+
+public class SimpleRule : IRuleNode
+{
+    public string PropertyKey { get; set; } = default!;
+    public string Operator { get; set; } = default!;
+    public object Value { get; set; } = default!;
+
+    public bool Evaluate(Dictionary<string, object> context, RuleEvaluator evaluator)
+        => evaluator.Evaluate(this, context);
+}
